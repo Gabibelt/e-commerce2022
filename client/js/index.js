0065ebe464a4c7df270a4d7eb3e8527e610bb491
Gabriel <gabrielbeltrando@gmail.com>
2023-10-06 08:37:31 -0300

    'JavaScript e-commerce'

--- client/js/index.js
+++ client/js/index.js
@@ -1,8 +1,8 @@
-const shopContent = document.getElementById("shopContent");
-const cart = []; //Este es un array vacio, nuestro carrito
+const shopContent= document.getElementById("shopContent");
+const cart= [];
 
 productos.forEach((product) =>{
-    const content = document.createElement("div");
+    const content= document.createElement("div");
     content.innerHTML = `
     <img src="${product.img}">
     <h3>${product.productName}</h3>
@@ -11,18 +11,34 @@ productos.forEach((product) =>{
     shopContent.append(content);
 
     const buyButton = document.createElement("button");
-    buyButton.innerText = "Comprar";
+    buyButton.innerText="comprar";
 
-    content.append(buyButton);
+content.append(buyButton);
 
-    buyButton-addEventListener("click", ()=>{
-        cart.push({
-            id: product.id,
-            productName: product.productName,
-            price: product.price,
-            quanty: product.quanty,
-            img: product.img,
-        })
-        console.log(cart)
-    })
+buyButton.addEventListener("click",()=>{
+    const repeat = cart.some((repeatProduct)=>repeatProduct.id === product.id);
+    
+    if(repeat){
+        cart.map((prod)=>{
+            if(prod.id===product.id){
+                prod.quanty++;
+                displayCarCounter();
+        }
+     });
+
+    }else{
+
+    cart.push({
+        id:product.id,
+        productName: product.productName,
+        price: product.price,
+        quanty:product.quanty,
+        img: product.img,
+    
+        });
+        displayCarCounter();
+      }
+    
+    });   
 });
+
